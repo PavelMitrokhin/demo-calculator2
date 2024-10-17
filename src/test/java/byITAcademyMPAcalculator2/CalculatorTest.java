9b8ad6ac3f980ca73cff85d9f5de0b9246c956bf
Pavel <pamitrovel@gmail.com>
2024-10-17 20:33:23 +0300

    refactor CalculatorTest, add sum1

--- src/test/java/byITAcademyMPAcalculator2/CalculatorTest.java
+++ src/test/java/byITAcademyMPAcalculator2/CalculatorTest.java
@@ -1,4 +1,13 @@
 package byITAcademyMPAcalculator2;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CalculatorTest {
+    @Test
+    public void sum1() {
+        Calculator calc = new Calculator();
+        double actual = calc.sum(1.3, 2.3);
+        Assertions.assertEquals(3.6, actual, 0.0001);
+    }
 }
