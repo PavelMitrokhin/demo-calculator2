3fbab9ad70308bc091639843e35143e6b73148e4
Pavel <pamitrovel@gmail.com>
2024-10-17 21:40:10 +0300

    refactor Main, add navigate to Onliner

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,13 @@
 import byITAcademyMPAcalculator2.Calculator;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        System.out.println(calculator.sum(2.33, 4.28));
+//        Calculator calculator = new Calculator();
+//        System.out.println(calculator.sum(2.33, 4.28));
+
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.navigate().to("https://www.onliner.by/");
     }
 }
