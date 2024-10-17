6e51d33efb4e5e41928e8842e85e3bba82d8cf80
Pavel <pamitrovel@gmail.com>
2024-10-17 20:44:27 +0300

    add Main

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import byITAcademyMPAcalculator2.Calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.sum(2.33, 4.28));
+    }
+}
