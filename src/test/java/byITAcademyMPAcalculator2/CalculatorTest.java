454aaa38b8e9b0a72c4649db7d1888b16d96b5e5
Pavel <pamitrovel@gmail.com>
2024-10-17 21:00:23 +0300

    refactor test, less lines

--- src/test/java/byITAcademyMPAcalculator2/CalculatorTest.java
+++ src/test/java/byITAcademyMPAcalculator2/CalculatorTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.Test;
 public class CalculatorTest {
     @Test
     public void sum1() {
-        Calculator calc = new Calculator();
-        double actual = calc.sum(1.3, 2.3);
-        Assertions.assertEquals(3.6, actual, 0.0001);
+        Assertions.assertEquals(3.6, new Calculator().sum(1.3,2.3), 0.0001);
     }
 }
